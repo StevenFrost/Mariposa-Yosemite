b3fd33e37c0f5928315f9183d906c84877cb69fb
Steven Frost <steve.frost.private@gmail.com>
2016-03-15 12:56:17 +0000

    Removing unused project references.

--- Source/Common/Utils/Source/UtilsPch.cpp
+++
@@ -1,9 +0,0 @@
-/************************************************************
-*                                                           *
-*   Copyright (C) Steven Frost 2016. All Rights Reserved.   *
-*                                                           *
-************************************************************/
-
-#include "UtilsPch.h"
-
-bool AssertEnableFastFail = false;
